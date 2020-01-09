a11f78398a82dc4cd880d11fa09795e9f6f267a1
Admin <elierpelegrin@gmail.com>
2020-01-09 12:37:17 -0300

    Creada la Class Carro, Motor y Direccion

---
+++ oo/carro.py
@@ -0,0 +1,85 @@
+
+# EJEMPO DE COMPOCICION DE CLASES DONDE UNA CLASE CARRO TIENE COMO PARAMETRO DOS CLASES -MOTOR Y - DIRECCION
+
+class Carro:
+    def __init__(self, motor, direccion):
+        self.direccion = direccion
+        self.motor = motor
+
+    def calculo_velocidad(self):
+        return self.motor.velocidad
+    def acelerando(self):
+        return self.motor.acelerar()
+    def frenar(self):
+        return self.motor.frenado()
+
+    def direccio(self):
+        return self.direccion.result
+
+
+
+class Motor:
+    #velocidad = 0
+    def __init__(self, velocidad=0):
+        self.velocidad = velocidad
+
+    def aceleracio(self):
+        if self.velocidad <= 199:
+            self.velocidad += 1
+
+
+    def frenado(self):
+         self.velocidad -= 2
+         self.velocidad = max(0,  self.velocidad)
+
+class Direccion:
+   position = 0
+   result = 'Norte'
+   def __init__(self):
+       self.direccion = ('Norte','Este', 'Sur', 'Oeste')
+       self.position = 0
+   def giro_dereita(self):
+        if self.position == 3:
+            self.position = 0
+        else:
+            self.position += 1
+        Direccion.result = self.direccion[self.position]
+
+   def giro_izquierda(self):
+       if self.position == -3:
+           self.position = 0
+       else:
+           self.position -= 1
+       Direccion.result = self.direccion[self.position]
+
+m = Motor()
+d = Direccion()
+c = Carro(m, d)
+
+
+
+
+'''m = Motor()
+m.aceleracio()
+m.aceleracio()
+m.aceleracio()
+print(m.velocidad)
+m.frenado()
+m.frenado()
+print(m.velocidad)
+
+
+d = Direccion()
+print(d.result)
+d.giro_dereita()
+
+print(d.result)
+d.giro_dereita()
+
+print(d.result)
+d.giro_dereita()
+
+print(d.result)
+
+d.giro_izquierda()
+print(d.result)'''
--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,21 @@
 class Pesoa:
     # de esta manera definimos atributos de la clase
     olhos = 2
-   # con esta funcion definimos atributos de un objeto
+   # con esta funcion definimos atributos de un objeto(filhos, nome, edad), ademas esta funcion es un metodo del objeto
     def __init__(self, *filhos, nome = None, edad =32):
         self.nome = nome
         self.edad = edad
         self.filhos = list(filhos)
-
+# Metodo de un objeto o instancia
     def saludo(self):
         return f'Hola {id(self)}'
+
+# Metodos de la clase comoensan con los decoreiton (@), estos son usados para realizar operaciones independientes tanto de la clase como del objeto
+    @staticmethod
+    def metodo_statico():
+        return 42
+
+
 
 if __name__ == '__main__':
     elier = Pesoa(nome='Elier')
@@ -33,3 +40,7 @@ if __name__ == '__main__':
     print('Objeto diego: ', diego.olhos)
     elier.olhos = 1;
     print('Objeto elier: ', elier.olhos)
+
+    print("-------------------")
+    print(Pesoa.metodo_statico())
+    print(elier.metodo_statico())
