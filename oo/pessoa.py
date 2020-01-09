bd80cc88d294012547778e36fc22a6002f3b8784
Admin <elierpelegrin@gmail.com>
2020-01-08 22:03:12 -0300

    Creada la Class Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pesoa:
+    # de esta manera definimos atributos de la clase
+    olhos = 2
+   # con esta funcion definimos atributos de un objeto
     def __init__(self, *filhos, nome = None, edad =32):
         self.nome = nome
         self.edad = edad
@@ -10,13 +13,23 @@ class Pesoa:
 if __name__ == '__main__':
     elier = Pesoa(nome='Elier')
     diego = Pesoa(elier, nome="Diego", edad=65)
-    print(elier.nome)
-    print(elier.filhos)
-    print(elier.edad)
     print("--------")
-    print(diego.nome)
+    print('Atributos de los objetos "diego" y "elier')
+    print('Nombre: ', elier.nome)
+    print('Filhos: ', elier.filhos)
+    print('Edad: ', elier.edad)
+    print()
+    print('Nombre: ', diego.nome)
     for filhos in diego.filhos:
         print(type(filhos))
-        print(filhos.nome)
+        print('Filhos: ', filhos.nome)
         print(type(filhos.nome))
-    print(diego.edad)
+    print('Edad: ', diego.edad)
+
+
+    print("-------------------")
+    print('Atributo de la clase Pesoa: "olhos"')
+    print('Clae Pesoa: ', Pesoa.olhos)
+    print('Objeto diego: ', diego.olhos)
+    elier.olhos = 1;
+    print('Objeto elier: ', elier.olhos)
