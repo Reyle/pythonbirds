75733ccbe142f7b6a51d947b435122d0ed6d7685
Admin <elierpelegrin@gmail.com>
2020-01-08 21:38:44 -0300

    Creada la Class Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,22 @@
 class Pesoa:
-    def __init__(self, nome = None):
+    def __init__(self, *filhos, nome = None, edad =32):
         self.nome = nome
+        self.edad = edad
+        self.filhos = list(filhos)
 
     def saludo(self):
         return f'Hola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pesoa('Elier')
-    print(p.saludo())
-    print(id(p))
-    print()
-    print(p.nome)
+    elier = Pesoa(nome='Elier')
+    diego = Pesoa(elier, nome="Diego", edad=65)
+    print(elier.nome)
+    print(elier.filhos)
+    print(elier.edad)
+    print("--------")
+    print(diego.nome)
+    for filhos in diego.filhos:
+        print(type(filhos))
+        print(filhos.nome)
+        print(type(filhos.nome))
+    print(diego.edad)
